87814f8f061dcd66939e8de794fe91e359346b15
JulieGunawan <julie.gunawan@protonmail.com>
2024-01-08 16:24:44 -0700

    adding webpack

--- src/fpcpp20.tsx
+++ src/fpcpp20.tsx
@@ -3,9 +3,9 @@ import React from 'react'
 // import Reveal from 'reveal.js'
 // import Markdown from 'reveal.js/plugin/markdown/markdown.js'
 
-// import 'reveal.js/dist/reveal.css'
-// import 'reveal.js/dist/theme/solarized.css'
-// import 'highlight.js/styles/intellij-light.css'
+import 'reveal.js/dist/reveal.css'
+import 'reveal.js/dist/theme/solarized.css'
+import 'highlight.js/styles/intellij-light.css'
 // import RevealHighlight from 'reveal.js/plugin/highlight/highlight.js'
 // import { Slide, Renderer } from './slides'
 
